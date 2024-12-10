83026ab5dfc960c98dcb702508654b45dc2529a4
Victor Eduardo <victorepc.dev@gmail.com>
2024-12-10 01:24:05 -0300

    Add new functions and test.

--- interfaces/list.h
+++ interfaces/list.h
@@ -3,17 +3,22 @@
 #define LIST_H
 
 typedef struct list List;
-typedef struct node Node;
 
 List* lst_create();
 int lst_isEmpty(List* list);
 List* lst_add(List* list, int value);
-int lst_search(List* list, int value);
+List* lst_search(List* list, int value);
 List* lst_delete(List* list, int value);
 void lst_print(List* list);
 void lst_destroy(List* list);
 
 int lst_length(List* list);
 int lst_less_than(List* list, int value);
+int lst_sum(List* list);
+int lst_prime(List* list);
+List* lst_concat(List* list1, List* list2);
+List* lst_diff(List* list1, List* list2);
+
+void lst_execute_test();
 
 #endif //LIST_H
