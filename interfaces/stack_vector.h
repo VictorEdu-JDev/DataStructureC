2356b2981f0b56961502dae8405d5f3bf44db7a0
Victor Eduardo <victorepc.dev@gmail.com>
2024-12-10 01:25:07 -0300

    Add new functions.

--- interfaces/stack_vector.h
+++ interfaces/stack_vector.h
@@ -2,13 +2,19 @@
 #ifndef STACK_VECTOR_H
 #define STACK_VECTOR_H
 
-typedef struct stack_vector Stack;
+typedef struct stack_vector Stack_Vector;
 
-Stack* stk_vector_create();
-int stk_vector_is_empty(Stack* s);
-void stk_vector_push(Stack* stack, int value);
-int stk_vector_pop(Stack* stack);
-void stk_vector_print(Stack* stack);
-void stk_vector_destroy(Stack* stack);
+Stack_Vector* stk_vector_create();
+int stk_vector_is_empty(Stack_Vector* s);
+void vector_push(Stack_Vector* stack, int value);
+int vector_pop(Stack_Vector* stack);
+void stk_vector_print(Stack_Vector* stack);
+void stk_vector_destroy(Stack_Vector* stack);
+
+int get_vector_top(Stack_Vector* stack);
+int odds_vector(Stack_Vector* stack);
+Stack_Vector* push_even_vector(Stack_Vector* p1, Stack_Vector* p2);
+
+void stk_execute_test();
 
 #endif //STACK_VECTOR_H
