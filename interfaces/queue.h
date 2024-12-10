e18111acda0f9c10b93b55df7715b2fa0915a6f1
Victor Eduardo <victorepc.dev@gmail.com>
2024-12-10 01:24:41 -0300

    Add new functions.

--- interfaces/queue.h
+++ interfaces/queue.h
@@ -12,4 +12,10 @@ int dequeue(Queue* queue);
 void que_print(Queue* queue);
 void que_destroy(Queue* queue);
 
+int count_greater(Queue* queue, int n);
+Queue* reverse_queue(Queue* queue);
+int count_even(Queue* queue);
+
+void que_execute_test();
+
 #endif //QUEUE_H
