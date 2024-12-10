fa1f24cfd3efc55a10b7e6ecc396f08bfbcecb68
Victor Eduardo <victorepc.dev@gmail.com>
2024-12-10 01:23:48 -0300

    Fixes numerous bugs, restructures, and reimplements various functions.

--- implementations/list.c
+++ implementations/list.c
@@ -2,14 +2,9 @@
 #include <stdlib.h>
 #include "../interfaces/list.h"
 
-struct node {
-    int data;
-    Node* next;
-};
-
 struct list {
-    Node* head;
-    int length;
+    int data;
+    List* next;
 };
 
 List* lst_create() {
@@ -21,35 +16,25 @@ int lst_isEmpty(List* list) {
 }
 
 List* lst_add(List* list, int data) {
-    if (list == NULL) {
-        list = (List*)malloc(sizeof(List));
-        list->head = NULL;
-        list->length = 0;
-    }
-
-    Node* newNode = (Node*)malloc(sizeof(Node));
+    List* newNode = (List*)malloc(sizeof(List));
     newNode->data = data;
-    newNode->next = list->head;
-
-    list->head = newNode;
-    list->length++;
-
-    return list;
+    newNode->next = list;
+    return newNode;
 }
 
-int lst_search(List* list, int data) {
-    Node* current = list->head;
+List* lst_search(List* list, int data) {
+    List* current = list;
     while (current != NULL) {
         if (current->data == data) {
-            return 1;
+            return current;
         }
         current = current->next;
     }
-    return 0;
+    return NULL;
 }
 
 void lst_print(List* list) {
-    Node* current = list->head;
+    List* current = list;
     while (current != NULL) {
         printf("%d ", current->data);
         current = current->next;
@@ -58,54 +43,166 @@ void lst_print(List* list) {
 }
 
 List* lst_delete(List* list, int data) {
-    if (list == NULL || list->head == NULL) return NULL;
+    if (list == NULL) return NULL;
 
-    if (list->head->data == data) {
-        Node* temp = list->head->next;
-        free(list->head);
-        list->head = temp;
-        list->length--;
+    if (list->data == data) {
+        List* temp = list;
+        list = list->next;
+        free(temp);
         return list;
     }
 
-    Node* temp = list->head->next;
-    Node* tempBefore = list->head;
-
-    while (temp != NULL) {
-        if (temp->data == data) {
-            tempBefore->next = temp->next;
+    List* current = list;
+    while (current->next != NULL) {
+        if (current->next->data == data) {
+            List* temp = current->next;
+            current->next = current->next->next;
             free(temp);
-            list->length--;
             return list;
         }
-        tempBefore = temp;
-        temp = temp->next;
+        current = current->next;
     }
-
     return list;
 }
 
 void lst_destroy(List* list) {
-    Node* temp = NULL;
-    while (list != NULL && list->head != NULL) {
-        temp = list->head;
-        list->head = list->head->next;
+    List* current = list;
+    List* temp = NULL;
+    while (current != NULL) {
+        temp = current;
+        current = current->next;
         free(temp);
     }
-    free(list);
 }
 
 int lst_length(List* list) {
-    if (list == NULL) return 0;
-    return list->length;
+    int length = 0;
+    List* current = list;
+    while (current != NULL) {
+        length++;
+        current = current->next;
+    }
+    return length;
 }
 
 int lst_less_than(List* list, int value) {
-    Node* current = list->head;
     int count = 0;
+    List* current = list;
     while (current != NULL) {
-        if (current->data < value) count ++;
+        if (current->data < value) {
+            count++;
+        }
         current = current->next;
     }
     return count;
+}
+
+int lst_sum(List* list) {
+    int sum = 0;
+    List* current = list;
+    while (current != NULL) {
+        sum += current->data;
+        current = current->next;
+    }
+    return sum;
+}
+
+int lst_prime(List* list) {
+    int primeCount = 0;
+    List* current = list;
+
+    while (current != NULL) {
+        int num = current->data;
+        if (num < 2) {
+            current = current->next;
+            continue;
+        }
+
+        int isPrime = 1;
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                isPrime = 0;
+                break;
+            }
+        }
+
+        if (isPrime) {
+            primeCount++;
+        }
+        current = current->next;
+    }
+
+    return primeCount;
+}
+
+List* lst_concat(List* list1, List* list2) {
+    if (list1 == NULL) return list2;
+    if (list2 == NULL) return list1;
+
+    List* newList = NULL;
+    List* current = list1;
+    while (current != NULL) {
+        newList = lst_add(newList, current->data);
+        current = current->next;
+    }
+
+    current = list2;
+    while (current != NULL) {
+        newList = lst_add(newList, current->data);
+        current = current->next;
+    }
+
+    return newList;
+}
+
+List* lst_diff(List* list1, List* list2) {
+    if (list1 == NULL || list2 == NULL) return list1;
+
+    List* diff = NULL;
+
+    List* current1 = list1;
+
+    while (current1 != NULL) {
+        if (!lst_search(list2, current1->data)) {
+            diff = lst_add(diff, current1->data);
+        }
+        current1 = current1->next;
+    }
+
+    return diff;
+}
+
+void lst_execute_test() {
+    List* l1 = lst_create();
+    l1 = lst_add(l1, 60);
+    l1 = lst_add(l1, 13);
+    l1 = lst_add(l1, 45);
+    l1 = lst_add(l1, 28);
+    l1 = lst_add(l1, 41);
+    l1 = lst_add(l1, 37);
+
+    l1 = lst_delete(l1, 41);
+    lst_print(l1);
+
+    printf("Num. nos c/ info < que 30: %d\n", lst_less_than(l1, 30));
+    printf("O comprimento da lista eh %d\n", lst_length(l1));
+    printf("Soma dos valores dos nos %d\n", lst_sum(l1));
+    printf("Num. nos com val. primos eh %d\n", lst_prime(l1));
+
+    List* l2 = lst_create();
+    l2 = lst_add(l2, 28);
+    l2 = lst_add(l2, 45);
+    l2 = lst_add(l2, 130);
+
+    List* l3 = lst_concat(l1, l2);
+    lst_print(l3);
+
+    l1 = lst_diff(l1, l2);
+    lst_print(l1);
+
+    lst_destroy(l1);
+    lst_destroy(l2);
+    lst_destroy(l3);
+
+    system("PAUSE");
 }
